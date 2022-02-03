499b450a5f7ac35044d6071e97e5e73d247f624b
mateus oliveira <mateus.gald@gmail.com>
2022-02-03 17:06:02 -0300

    projeto created

---
+++ SELLERMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SELLERMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
